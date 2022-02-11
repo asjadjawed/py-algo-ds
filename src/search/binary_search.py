a4842591b6b186177d810f3ee8c9919beebd1d37
Asjad Jawed Siddiqui <asjadjawed@gmail.com>
2022-02-11 17:34:44 +0000

    algo: binary search recursive

--- src/search/binary_search.py
+++ src/search/binary_search.py
@@ -18,3 +18,26 @@ def binary_search(list, item):
         else:
             low = mid + 1
     return None
+
+
+def binary_search_recursive(list, item, low=0, high=None):
+    if len(list) == 0:
+        return None
+
+    if item == None:
+        return None
+
+    if high == None:
+        high = len(list) - 1
+
+    mid = (high + low) // 2
+    guess = list[mid]
+
+    if item == guess:
+        return mid
+    if high < low:
+        return None
+    if item < guess:
+        return binary_search_recursive(list, item, low, mid-1)
+    if item > guess:
+        return binary_search_recursive(list, item, mid+1, high)
--- src/tests/test_binary_search.py
+++ src/tests/test_binary_search.py
@@ -1,5 +1,5 @@
 import unittest
-from search.binary_search import binary_search
+from search.binary_search import binary_search, binary_search_recursive
 
 
 class TestBinarySearch(unittest.TestCase):
@@ -15,6 +15,20 @@ class TestBinarySearch(unittest.TestCase):
         self.assertEqual(binary_search([1, 3, 5,  7, 9], 11), None)
         self.assertEqual(binary_search([-1, 3, 5,  7, 9], -1), 0)
         self.assertEqual(binary_search([-1, 3, 5,  7, 9], -11), None)
+
+    def test_binary_search_recursive(self):
+        '''Test Binary Search Recursive'''
+        self.assertEqual(binary_search_recursive([], None), None)
+        self.assertEqual(binary_search_recursive([5], None), None)
+        self.assertEqual(binary_search_recursive([], 5), None)
+        self.assertEqual(binary_search_recursive([5], 5), 0)
+        self.assertEqual(binary_search_recursive([5], 10), None)
+        self.assertEqual(binary_search_recursive(
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 6), 5)
+        self.assertEqual(binary_search_recursive([1, 3, 5,  7, 9], 5), 2)
+        self.assertEqual(binary_search_recursive([1, 3, 5,  7, 9], 11), None)
+        self.assertEqual(binary_search_recursive([-1, 3, 5,  7, 9], -1), 0)
+        self.assertEqual(binary_search_recursive([-1, 3, 5,  7, 9], -11), None)
 
 
 if __name__ == "__main__":
