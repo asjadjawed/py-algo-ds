aa04eb420a6da53fe2a1d97242e33edf06a439bb
Asjad Jawed Siddiqui <asjadjawed@gmail.com>
2022-03-16 16:56:10 +0000

    fix: without len

--- src/recursion/count.py
+++ src/recursion/count.py
@@ -1,8 +1,6 @@
 def recursive_count(arr):
     if arr is None:
         return None
-    if len(arr) == 0:
+    if arr == []:
         return 0
-    if len(arr) == 1:
-        return 1
     return 1 + recursive_count(arr[1:])
