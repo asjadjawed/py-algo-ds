0fe39c56afa275584df3833684c740c3b5eafc76
Asjad Jawed Siddiqui <asjadjawed@gmail.com>
2024-05-20 21:19:52 +0500

    feat: binary search update

--- py_algo_ds/algorithms/binary_search.py
+++ py_algo_ds/algorithms/binary_search.py
@@ -3,13 +3,13 @@ Binary Search
 """
 
 
-def search(iterable: list, item: object) -> int:
+def search(iterable: list[int], item: int) -> int:
     """
     Searches for the first occurrence of an element in a list in O(log(n))
 
     Args:
-        iterable (list): The list to search
-        item (object): The element to search for
+        iterable (list[int]): The list to search
+        item (int): The element to search for
 
     Returns:
         int: the index of the element if found else -1
@@ -17,27 +17,26 @@ def search(iterable: list, item: object) -> int:
     if not iterable:
         return -1
 
-    low = 0
-    high = len(iterable)
-    while low < high:
-        mid = (low + high) // 2
+    low, high = 0, len(iterable) - 1
+    while low <= high:
+        mid = low + (high - low) // 2
         pointer = iterable[mid]
         if pointer == item:
             return mid
         if pointer < item:
             low = mid + 1
         else:
-            high = mid
+            high = mid - 1
     return -1
 
 
-def search_recursion(iterable: list, item: object, start=None, end=None) -> int:
+def search_recursion(iterable: list[int], item: int, start=None, end=None) -> int:
     """
     Searches for the first occurrence of an element in a list in O(log(n))
 
     Args:
-        iterable (list): The list to search
-        item (object): The element to search for
+        iterable (list[int]): The list to search
+        item (int): The element to search for
         start (int, optional): Defaults to None. Auto-calculated.
         end (int, optional): Defaults to None. Auto-calculated.
 
@@ -55,7 +54,7 @@ def search_recursion(iterable: list, item: object, start=None, end=None) -> int:
     if start > end:
         return -1
 
-    mid = (start + end) // 2
+    mid = start + (end - start) // 2
     pointer = iterable[mid]
 
     if pointer == item:
