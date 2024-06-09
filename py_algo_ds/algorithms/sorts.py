214282e9abfa743e5f0717500d1ca2fa7c317b96
Asjad Jawed Siddiqui <asjadjawed@gmail.com>
2024-06-09 21:50:17 +0500

    feat: swap flag for bubble sort

--- py_algo_ds/algorithms/sorts.py
+++ py_algo_ds/algorithms/sorts.py
@@ -92,9 +92,13 @@ def bubble_sort(arr: list[int]) -> list[int]:
         [11, 12, 22, 25, 34, 64, 90]
     """
     for i in range(len(arr) - 1, 0, -1):
+        swapped = False
         for j in range(i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
     return arr
 
 
