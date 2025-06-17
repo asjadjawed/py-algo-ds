c6a3254f7669e80e481c0922e15de07ae4094159
Asjad Jawed Siddiqui <asjadjawed@gmail.com>
2025-06-17 22:50:51 +0500

    feat: move zeros problem

---
+++ py_algo_ds/problems/move_zeros.py
@@ -0,0 +1,23 @@
+def move_zeros(nums: list[int]) -> list[int]:
+    """
+    Moves all zeros in the provided list to the end, while maintaining the relative
+    order of non-zero elements.
+
+    Parameters:
+        nums (list[int]): The list of integers to be reordered.
+
+    Returns:
+        None: Modifies the list in-place without returning any value.
+
+    Example:
+        >>> nums = [1, 0, 2, 0, 0, 7]
+        >>> move_zeros(nums)
+        >>> print(nums)
+        [1, 2, 7, 0, 0, 0]
+    """
+    slow = 0
+    for fast in range(len(nums)):
+        if nums[fast] != 0:
+            nums[slow], nums[fast] = nums[fast], nums[slow]
+            slow += 1
+    return nums
---
+++ tests/problems/test_move_zeros.py
@@ -0,0 +1,19 @@
+import pytest
+from py_algo_ds.problems.move_zeros import move_zeros
+
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([1, 0, 2, 0, 0, 7], [1, 2, 7, 0, 0, 0]),
+        ([0, 1, 0, 3, 12], [1, 3, 12, 0, 0]),
+        ([0, 0, 1], [1, 0, 0]),
+        ([1, 0], [1, 0]),
+        ([0, 0, 0], [0, 0, 0]),
+        ([1, 2, 3], [1, 2, 3]),
+        ([], []),
+    ],
+)
+def test_move_zeros(nums: list[int], expected: list[int]):
+    move_zeros(nums)
+    assert nums == expected, f"Expected {expected}, but got {nums}"
